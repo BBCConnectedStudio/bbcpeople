740aba7c0b5a87a4763cd40c204c6019640b8b4f
Mohammad Forouzani <foroum02@mc-s056875.local>
2013-07-18 13:06:30 +0100

    Remove suppressing of exceptions so they can be picked up by Bugsnag

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,12 +5,12 @@ class ApplicationController < ActionController::Base
     raise ActionController::RoutingError.new('Not Found')
   end
 
-  unless Rails.application.config.consider_all_requests_local
-    rescue_from Exception, :with => :render_error
-    rescue_from ActiveRecord::RecordNotFound, :with => :render_not_found
-    rescue_from ActionController::RoutingError, :with => :render_not_found
-    rescue_from ActionController::UnknownController, :with => :render_not_found
-  end
+  #unless Rails.application.config.consider_all_requests_local
+    #rescue_from Exception, :with => :render_error
+    #rescue_from ActiveRecord::RecordNotFound, :with => :render_not_found
+    #rescue_from ActionController::RoutingError, :with => :render_not_found
+    #rescue_from ActionController::UnknownController, :with => :render_not_found
+  #end
 
   private
 
